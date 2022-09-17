f2181c8dedc3cec7ca6b41d68e664f143c8b5f5f
Paulo Geliandes <geliandes@hotmail.com>
2022-09-17 13:42:52 -0300

    create mobile app

--- mobile/src/components/Background/index.tsx
+++ mobile/src/components/Background/index.tsx
@@ -5,15 +5,15 @@ import backgroundImg from '../../assets/background-galaxy.png'
 import { styles } from './styles';
 
 interface Props {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
 
 export function Background({ children }: Props) {
   return (
     <ImageBackground 
         source={backgroundImg} 
+        defaultSource={backgroundImg}
         style={styles.container}
-        defaultSource={styles.container}
     >
         {children}
     </ImageBackground>
