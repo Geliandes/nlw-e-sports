5c1c11ffdf504187a5c02ee3dc18657bcb2d1fa1
Paulo Geliandes <geliandes@hotmail.com>
2022-09-18 10:56:50 -0300

    update

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import { StatusBar } from 'react-native';
 import {
   useFonts,
@@ -6,13 +7,17 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter'
+import {  } from 'expo'
+import { Subscription } from 'expo-modules-core'
 
 import { Home } from './src/screens/Home'
-
 import { Background } from './src/components/Background';
-
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+import './src/services/notificationConfigs'
+import { getPushNotificationToken } from './src/services/getPushNotificationToken'
+import * as Notifications from 'expo-notifications';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,6 +27,29 @@ export default function App() {
     Inter_900Black
   });
 
+  const getNotificationListener = useRef<Subscription>();
+  const reponseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+  getPushNotificationToken();
+});
+  useEffect(() => {
+    getNotificationListener.current = Notifications
+    .addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    reponseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if(getNotificationListener.current && reponseNotificationListener){
+        Notifications.removeNotificationSubscription(getNotificationListener)
+        Notifications.removeNotificationSubscription(reponseNotificationListener)
+      }
+    }
+  },[]);
 
 
   return (
---
+++ mobile/src/components/DuoMatch/index.tsx
@@ -0,0 +1,62 @@
+import { View, Modal, ModalProps, Text, TouchableOpacity, Alert, ActivityIndicator} from "react-native";
+import {Clipboard} from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
+import { Activity, Check, CheckCircle } from "phosphor-react-native";
+import { Heading } from "../../components/Heading";
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+import { useState } from "react";
+
+interface Props extends ModalProps {
+  discord: string;
+  onClose: () => void;
+}
+
+export function DuoMatch({ discord, onClose, ...rest }: Props) {
+
+    const [isCopping, setIsCopping] = useState(false)
+
+    function handleCopyDiscordToClipboard() {
+        setIsCopping(true) 
+        Clipboard.setString(discord)
+            
+        Alert.alert('Discord Copiado!', 'Usuário copiado para a área de transferência')
+        setIsCopping(false)
+    }
+
+  return (
+    <Modal animationType="fade" transparent statusBarTranslucent {...rest}>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
+            <MaterialIcons
+              name="close"
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
+          <CheckCircle size={64} color={THEME.COLORS.SUCCESS} weight="bold" />
+
+          <Heading
+            title="Let's play!"
+            subtitle="Agora é só começar a jogar!"
+            style={{ alignItems: "center", marginTop: 24 }}
+          />
+
+          <Text style={styles.label}>Adicione no Discord</Text>
+
+          <TouchableOpacity 
+          style={styles.discordButton}
+          onPress={handleCopyDiscordToClipboard}
+          disabled={isCopping}
+          >
+            <Text style={styles.discord}>
+                {isCopping ? <ActivityIndicator color={THEME.COLORS.PRIMARY}/> : discord}
+            </Text>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
